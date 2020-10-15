1e8eae2e65bdf6ce67765e1bdc77a8b5b1accb16
Mallikarjuna Maddi <mallikarjuna.maddi@ggktech.com>
2020-10-15 16:18:32 +0530

    Created a Material module.

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const MaterialComponents = [];
+
+@NgModule({
+  declarations: [MaterialComponents],
+  imports: [MaterialComponents]
+})
+export class MaterialModule { }
