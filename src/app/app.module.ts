c35317586ec8f1e6571174bce90fcca61ae96f57
Mallikarjuna Maddi <mallikarjuna.maddi@ggktech.com>
2020-10-15 16:22:55 +0530

    Added Core Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CoreModule } from './core/core.module';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
@@ -15,7 +16,8 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
