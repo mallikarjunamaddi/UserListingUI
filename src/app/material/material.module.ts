a39676d6a53e7be42e0488c2699d29f1f9a4d000
Mallikarjuna Maddi <mallikarjuna.maddi@ggktech.com>
2020-10-15 20:55:02 +0530

    Enabled Sort and added logic to feed datatable.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
- 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const MaterialComponents = [
   MatTableModule,
   MatSortModule,
+  MatProgressSpinnerModule
 ];
 
 @NgModule({
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { User } from '../core/models/user';
+import { UserService } from '../core/services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -9,15 +11,39 @@ import { User } from '../core/models/user';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  dataSource: MatTableDataSource<User>;
+  noDataFound: boolean;
   displayedColumns: string[];
+  dataSource: MatTableDataSource<User>;
 
-  constructor() { 
+  // When set {static : true}, Angular Compiler consider this @ViewChild element is static,
+  // so it only obtain the MatSort one time at ngOnInit(). Even if *ngIf triggered still can't
+  // catch the MatSort component. So use {static: false}
+  @ViewChild(MatSort, {static: false}) set sort(sort: MatSort) {
+    if (sort){
+      this.dataSource.sort = sort;
+    }
+  }
+
+  constructor(private userService: UserService) { 
     this.dataSource = new MatTableDataSource<User>();
     this.displayedColumns = ["name", "email", "roleType", "status", "edit"];
+    this.getUsers();
   }
 
   ngOnInit(): void {
   }
 
+  getUsers() {
+    this.userService.getUsers().subscribe((users) => {
+      if(users) {
+        this.noDataFound = false;
+        this.dataSource.data = users;
+      } else {
+        this.noDataFound = true;
+        this.dataSource.data = [];
+      }
+    }, (error) => {
+      console.error(error);
+    });
+  }
 }
